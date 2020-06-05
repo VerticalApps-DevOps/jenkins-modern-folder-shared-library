4b779c619263ac4d97e1db948ca53775de8fd318
Justin <jlea@verticalapps.com>
2020-06-05 15:13:22 -0400

    Missed the opening bracket of a loop :(

--- resources/orchPublish.ps1
+++ resources/orchPublish.ps1
@@ -56,7 +56,7 @@ $processes = $releasesjson.value
 
 Write-Output $processes
 
-foreach($i in $processes)
+foreach($i in $processes) {
    if ($i.ProcessKey -eq $release.ProcessKey) {
       Invoke-RestMethod -SkipCertificateCheck -Body $release "$env:url/odata/Releases($($i.Id))/UiPath.Server.Configuration.OData.UpdateToLatestPackageVersion" -Method Post -Authentication Bearer -Token ($tokenstring)
       $updated  = 1
