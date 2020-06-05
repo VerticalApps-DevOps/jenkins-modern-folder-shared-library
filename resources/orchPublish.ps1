e67e00c95cb35fa37305051486d2f74f10d49d78
Justin <jlea@verticalapps.com>
2020-06-05 15:47:46 -0400

    Different debugging output

--- resources/orchPublish.ps1
+++ resources/orchPublish.ps1
@@ -61,7 +61,8 @@ $processes = $releasesjson.value
 foreach($i in $processes) {
    if ($i.ProcessKey -eq $release.ProcessKey) {
       Write-Output "Beginning Process Update"
-      Invoke-RestMethod -SkipCertificateCheck -Body $specificPackageParameters "$env:url/odata/Releases($($i.Id))/UiPath.Server.Configuration.OData.UpdateToSpecificPackageVersion" -Method Post -Authentication Bearer -Token ($tokenstring)
+      $updateresponse = Invoke-RestMethod -SkipCertificateCheck -Body $specificPackageParameters "$env:url/odata/Releases($($i.Id))/UiPath.Server.Configuration.OData.UpdateToSpecificPackageVersion" -Method Post -Authentication Bearer -Token ($tokenstring)
+      Write-Output $updateresponse
       $updated  = 1
       Write-Output "Process Successfully Updated"
    }
